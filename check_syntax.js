7b2dedbead31e6eb433d6afcaacdd721e1de0702
Bhavin Kapadiya <bhavinkapadiya8@gmail.com>
2025-12-16 15:29:14 +0530

    chore: remove Trendyol scraper development and debugging files

--- check_syntax.js
+++
@@ -1,6 +0,0 @@
-try {
-    const ex = require('./src/scraper/extractor');
-    console.log('Extractor loaded successfully');
-} catch (e) {
-    console.error('Syntax Error:', e.message);
-}
--- script_7.js
+++
@@ -1,2 +0,0 @@
-
-    window.__STANDALONE = (window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) || (('standalone' in window.navigator) && (window.navigator.standalone))
